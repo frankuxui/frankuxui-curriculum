b5e03f1dd770d63d328919733fe05a223d7c5e89
frankuxui <fisdray@gmail.com>
2025-05-24 00:27:10 +0200

    github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  site: 'https://frankuxui-curriculum.github.io',
+  site: 'https://frankuxui.github.io/frankuxui-curriculum',
   base: '/frankuxui-curriculum',
   vite: {
     plugins: [tailwindcss()]
